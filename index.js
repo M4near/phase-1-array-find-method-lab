f47fe45b7718ca1b569d49577ff21d002dbccf9c
Michael Fournier <michaeljfournier5@gmail.com>
2022-02-24 17:31:38 -0500

    commit tree 674fc140441d08df1fe71735686dd38dc1759286

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWinCB(element){
+    return (element.result === "W")
+}
+
+function superbowlWin(games){
+   let res = games.find(superbowlWinCB)
+   return res === undefined ? res : res.year
+}
